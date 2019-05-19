e31d161befd7ccbba7b8d34ed51b3f057ea0388c
cocoy <cocoy_shuffle@me.com>
2019-05-19 17:35:47 -0500

    added routes for app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { CartComponent } from './cart/cart.component';
+
+const routes: Routes = [
+  {
+      path: '',
+      component: HomeComponent,
+  },
+  {
+    path: 'cart',
+    component: CartComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  declarations: []
 })
 export class AppRoutingModule { }
